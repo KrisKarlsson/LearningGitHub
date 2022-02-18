39a614b90b83db962dd0932cdbc49568c64caf0d
KrisKarlsson <kris.e.h.karlsson@gmail.com>
2022-02-18 14:53:26 +1100

    Initial Commit

---
+++ GitHub test/GitHub_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_testApp.swift
+//  GitHub test
+//
+//  Created by Kris Karlsson on 18/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
